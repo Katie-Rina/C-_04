5fa1b1304d28b5564ce25a44b2cef8d0ea701048
KatieRina <eashchukina@yandex.ru>
2022-11-11 01:44:16 +0300

    третья задача

--- HW29/Program.cs
+++ HW29/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов 
+// и выводит их на экран.
+
+int[] CreateArray(int n, int s, int f)
+{
+    int[] mass = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        mass[i] = new Random().Next(s, f + 1);
+    }
+    return mass;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+Console.Clear();
+int[] array = CreateArray(8, -100, 100);
+PrintArray(array);
