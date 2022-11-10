0235aa09cffc8d284c9a5a86496ad92c4aaebfdb
KatieRina <eashchukina@yandex.ru>
2022-11-11 01:19:03 +0300

    вторая задача

--- HW27/Program.cs
+++ HW27/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 27: Напишите программу, которая принимает на вход число 
+// и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int NumbSumm(int numb)
+{
+    int summ = 0;
+    while (numb >= 1)
+    {
+        summ += numb % 10;
+        numb = numb / 10;
+    }
+    return summ;
+}
+
+Console.Clear();
+Console.Write("Введите натуральное число: ");
+int numb = Convert.ToInt32(Console.ReadLine());
+while (numb <= 0)
+{
+    Console.WriteLine ("Число должно быть натуральным!!! Введите ещё раз: ");
+    numb = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine($"Сумма цифр числа равна {NumbSumm(numb)}");
