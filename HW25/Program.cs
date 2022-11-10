7d4eeb985fff7ae5e26fee63f4faa5dde2d64c89
KatieRina <eashchukina@yandex.ru>
2022-11-11 00:52:06 +0300

    первая задача

--- HW25/Program.cs
+++ HW25/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 25: 
+// Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+double PowerNumber(double A, int B)
+{
+    double mult = 1;
+    int i = 1;
+    while (i <= B)
+    {
+        mult *= A;
+        i += 1;
+    }
+    return mult;
+}
+
+Console.Clear();
+Console.Write("Введите число А: ");
+double A = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите натуральное число B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+while (B <= 0)
+{
+    Console.WriteLine ("Число В должно быть натуральным!!! Введите ещё раз: ");
+    B = Convert.ToInt32(Console.ReadLine());
+}
+double pow = PowerNumber(A, B);
+Console.WriteLine(pow);
